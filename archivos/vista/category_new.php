1888c8b54ab2e23e7af9e466d2f868d54a8190d4
monicasofia201 <69876939+monicasofia201@users.noreply.github.com>
2024-08-09 08:06:55 -0500

    Biblioteca-Category-HTML/CSS

--- archivos/vista/category_new.php
+++ archivos/vista/category_new.php
@@ -22,69 +22,8 @@ if (isset($_SESSION['id_userprofile'])){
     <script src="../../herramientas/js/biblioteca.js"></script>
     <link rel="stylesheet" href="../../herramientas/css/style.css">
 
+    <link rel="stylesheet" href="../vista/css/category_new.css">
 
-
-    <style>
-    .subtitle {
-        color: #555;
-        /* Cambiar el color del subtítulo */
-        font-size: 1.5em;
-        /* Ajustar el tamaño del subtítulo */
-        margin-bottom: 1.5em;
-        /* Ajustar el espacio inferior del subtítulo */
-    }
-
-    .input {
-        border-radius: 5px;
-        /* Agregar bordes redondeados a los campos de entrada */
-    }
-
-    .button {
-        border-radius: 25px;
-        /* Agregar bordes redondeados al botón */
-    }
-
-    .button:hover {
-        background-color: #48a9a6;
-        /* Cambiar el color de fondo al pasar el mouse */
-    }
-
-    .button:active {
-        background-color: #3b8070;
-        /* Cambiar el color de fondo al hacer clic */
-    }
-
-    .create-button {
-        background-color: #4CAF50;
-        color: white;
-    }
-
-    .close-button {
-        background-color: #f44336;
-        color: white;
-    }
-
-    .cabecera_menu {
-        position: relative;
-    }
-
-    .fixed-top-right {
-        position: absolute;
-        top: 10px;
-        /* Ajusta este valor según necesites */
-        right: 10px;
-        /* Ajusta este valor según necesites */
-        z-index: 1000;
-        /* Asegura que esté por encima de otros elementos */
-        padding: 5px 10px;
-        /* Espaciado interno */
-    }
-
-    .fixed-top-right .btn i {
-        margin-right: 5px;
-        /* Espacio entre el icono y el texto */
-    }
-    </style>
 </head>
 
 <body>
@@ -113,48 +52,48 @@ if (isset($_SESSION['id_userprofile'])){
             <div class="content__page">
 
 
-
-                <div class=" pb-6 pt-6">
-                    <form id="addSectionForm" class="container">
-                        <div class=" is-fluid mb-6">
-                            <h1 class="title">Biblioteca Bilingüismo<br>Secciones B-Team-Language </h1>
-                        </div>
-                        <div class="columns container">
-                            <div class="column ">
-                                <div class="field ">
-                                    <label class="label">Nombre de la Sección a Ingresar:</label>
-                                    <div class="control">
-                                        <input class="input" type="text" id="nombre"
-                                            pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{4,50}" maxlength="50" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="column">
-                                <div class="field">
-                                    <label class="label">Descripción:</label>
-                                    <div class="control">
-                                        <input class="input" type="text" id="descripcion"
-                                            pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{5,150}" maxlength="150">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="column">
-                                <div class="field">
-                                    <label class="label">Idioma:</label>
-                                    <div class="control">
-                                        <select name="" id="id_idioma"></select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <p class="control has-text-centered">
-                                    <button type="button" id="guardar_seccion" class="create-button">Guardar</button>
-                                </p>
-                            </div>
-                        </div>
-
-                    </form>
+            <div class="pb-6 pt-6">
+    <div class="is-fluid mb-6">
+        <h1 class="title">Biblioteca Bilingüismo<br>Secciones B-Team-Language</h1>
+    </div>
+    <div class="columns container">
+        <div class="column is-full">
+            <div class="field">
+                <label class="label">Nombre de la Sección a Ingresar:</label>
+                <div class="control">
+                    <input class="input" type="text" id="nombre"
+                        pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{4,50}" maxlength="50" required>
                 </div>
+            </div>
+            <div class="field">
+                <label class="label">Idioma:</label>
+                <div class="control">
+                    <select class="select" name="" id="id_idioma"></select>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Descripción:</label>
+                <div class="control">
+                    <textarea class="textarea" id="descripcion"
+                        pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{5,150}" maxlength="150"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="field">
+        <p class="control has-text-centered">
+            <button type="button" id="guardar_seccion" class="create-button">Guardar</button>
+        </p>
+    </div>
+</div>
+
+
+
+
+
+
+
+            
 </body>
 
 </html>
